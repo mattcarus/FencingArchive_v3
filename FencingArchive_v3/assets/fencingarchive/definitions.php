1b65247ffb3ba16cacddcfb1146c22a3917f69b7
Matt Carus <mattcarus@gmail.com>
2015-02-03 23:29:43 +0000

    123

--- FencingArchive_v3/assets/fencingarchive/definitions.php
+++ FencingArchive_v3/assets/fencingarchive/definitions.php
@@ -7,7 +7,7 @@
 	define("DB_HOST", "localhost");
 	define("DB_USERNAME", "fencingarchive");
 	define("DB_PASSWORD", "UH3Rt0dnhQoqVz0aomqH");
-	define("DB_DATABASE", "fencingarchive");
+	define("DB_DATABASE", "FencingArchive_v3");
 	
 	
 ?>
